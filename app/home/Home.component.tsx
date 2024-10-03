3ea5b4fe3bea4fe4e3801a5cb5b995816f4b0892
ben wainaina <bwainainamwangi@gmail.com>
2024-10-03 14:57:25 +0300

    a bit of tydying up

--- app/home/Home.component.tsx
+++ app/home/Home.component.tsx
@@ -1,24 +1,35 @@
-import { Image, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import LogoPng from "../../assets/images/logo.png";
 
 export const HomeComponent = () => {
   return (
-    <View
-      style={{
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100%",
-        rowGap: 128,
-        backgroundColor: "white",
-      }}
-    >
-      <View style={{ alignItems: "center", rowGap: 12 }}>
-        <Text style={{ fontSize: 16 }}>Welcome back</Text>
-        <Text style={{ fontSize: 24, fontWeight: "bold" }}>Benson</Text>
+    <View style={styles.wrapper}>
+      <View style={styles.wrapper__section_one}>
+        <Text style={styles.wrapper__section_one__salutation}>
+          Welcome back
+        </Text>
+        <Text style={styles.wrapper__section_one__username}>Benson</Text>
       </View>
       <View>
-        <Image source={LogoPng} style={{ width: 120, height: 120 }} />
+        <Image source={LogoPng} style={styles.wrapper__logo} />
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  wrapper: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100%",
+    rowGap: 128,
+    backgroundColor: "white",
+  },
+  wrapper__section_one: {
+    alignItems: "center",
+    rowGap: 12,
+  },
+  wrapper__section_one__salutation: { fontSize: 16 },
+  wrapper__section_one__username: { fontSize: 24, fontWeight: "bold" },
+  wrapper__logo: { width: 120, height: 120 },
+});
